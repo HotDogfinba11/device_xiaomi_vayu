d6b143ad10bb992d362ad8d4794c8c4e2db2da9d
Sultan Alsawaf <sultan@kerneltoast.com>
2020-04-18 23:20:50 -0700

    vayu: Force triple frame buffers
    This yields significantly reduced jitter and doesn't have any measurable
    impact on latency.
    
    Change-Id: I4d1de21487757be3df3bd908e1a6152fd951d732

--- product_prop.mk
+++ product_prop.mk
@@ -15,7 +15,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	debug.sf.disable_backpressure=1 \
 	debug.sf.enable_hwc_vds=1	\
-	debug.hwui.renderer=skiavk
+	debug.hwui.renderer=skiavk \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
